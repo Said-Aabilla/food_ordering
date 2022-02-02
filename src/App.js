911c797a3b7662040d174580a8053cbd010ea05a
Said-Aabilla <said160aabilla@gmail.com>
2022-02-02 16:25:06 +0100

    list of meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header/>
+      <Meals/>
     </>
   );
 }
