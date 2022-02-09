82039fffa5fd0b66cb4e78356c473bb4ba2defb4
Said-Aabilla <said160aabilla@gmail.com>
2022-02-09 12:37:43 +0100

    submitting cart data to the backend

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -1,12 +1,19 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useContext } from "react";
 
-import classes from './Checkout.module.css';
+import classes from "./Checkout.module.css";
+import useHttp from "../../../hooks/use-http";
+import CartContext from "../../../store/cart/cart-context";
 
 //helpers to validate user inputs
-const isEmpty = (value) => value.trim() === '';
+const isEmpty = (value) => value.trim() === "";
 const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
+  //Custom hook to send checkout
+  const { isLoading, error, sendRequest: sendCheckout } = useHttp();
+
+  //cart context
+  const cartContext = useContext(CartContext);
 
   //state of the inputs validity
   const [formInputsValidity, setFormInputsValidity] = useState({
@@ -57,48 +64,72 @@ const Checkout = (props) => {
     }
 
     // Submit cart data
-    console.log(enteredName,enteredStreet,enteredPostalCode )
+    //construct the body
+    const checkoutBody = {
+      id: Math.random().toString,
+      meals: cartContext.items,
+      client: {
+        name: enteredName,
+        street: enteredStreet,
+        city: enteredCity,
+        postalCode: enteredPostalCode,
+      },
+    };
+    (sendCheckout(
+      {
+        url: "https://react-http-a0d8b-default-rtdb.firebaseio.com/checkouts.json",
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: checkoutBody
+      },
+      () => {}
+    ))();
+
+    
   };
 
   const nameControlClasses = `${classes.control} ${
-    formInputsValidity.name ? '' : classes.invalid
+    formInputsValidity.name ? "" : classes.invalid
   }`;
   const streetControlClasses = `${classes.control} ${
-    formInputsValidity.street ? '' : classes.invalid
+    formInputsValidity.street ? "" : classes.invalid
   }`;
   const postalCodeControlClasses = `${classes.control} ${
-    formInputsValidity.postalCode ? '' : classes.invalid
+    formInputsValidity.postalCode ? "" : classes.invalid
   }`;
   const cityControlClasses = `${classes.control} ${
-    formInputsValidity.city ? '' : classes.invalid
+    formInputsValidity.city ? "" : classes.invalid
   }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
+      {error && <p>{error}</p>}
       <div className={nameControlClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={nameInputRef} />
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
         {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
       <div className={streetControlClasses}>
-        <label htmlFor='street'>Street</label>
-        <input type='text' id='street' ref={streetInputRef} />
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
         {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
       <div className={postalCodeControlClasses}>
-        <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' ref={postalCodeInputRef} />
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
         {!formInputsValidity.postalCode && (
           <p>Please enter a valid postal code (5 characters long)!</p>
         )}
       </div>
       <div className={cityControlClasses}>
-        <label htmlFor='city'>City</label>
-        <input type='text' id='city' ref={cityInputRef} />
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
         {!formInputsValidity.city && <p>Please enter a valid city!</p>}
       </div>
       <div className={classes.actions}>
-        <button type='button' onClick={props.onCancel}>
+        <button type="button" onClick={props.onCancel}>
           Cancel
         </button>
         <button className={classes.submit}>Confirm</button>
